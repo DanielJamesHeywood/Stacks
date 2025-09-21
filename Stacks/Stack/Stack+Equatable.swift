6fd8643bc449f513b709e433efe01d869fa18c98
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-09-21 21:55:09 +0100

    Fixed == implementation by accounting for stacks of different sizes.

--- Stacks/Stack/Stack+Equatable.swift
+++ Stacks/Stack/Stack+Equatable.swift
@@ -3,6 +3,9 @@ extension Stack: Equatable where Element: Equatable {
     
     @inlinable
     public static func == (lhs: Stack, rhs: Stack) -> Bool {
+        guard lhs.count == rhs.count else {
+            return false
+        }
         for (lhElement, rhElement) in zip(lhs, rhs) {
             guard lhElement == rhElement else {
                 return false
