25044bd4d2283c98b5b016d9d1c6ac7c105ca8e1
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-09-21 16:24:04 +0100

    Implemented _handle variable for Index.

--- Stacks/Stack/Stack.Index/Stack.Index.swift
+++ Stacks/Stack/Stack.Index/Stack.Index.swift
@@ -4,6 +4,9 @@ extension Stack {
     public struct Index {
         
         @usableFromInline
+        internal unowned var _handle: _Node?
+        
+        @usableFromInline
         internal var _offset: Int
         
         @usableFromInline
